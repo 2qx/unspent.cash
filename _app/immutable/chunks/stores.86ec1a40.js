dbbc7e5bd47628262f965d0645abe6e410a6286d
2qx <2qx@users.noreply.github.com>
2023-09-20 01:10:44 +0000

    deploy: 2qx/unspent@891c9c1b024ff31a41514f81653c0e0b816feb35

--- _app/immutable/chunks/stores.86ec1a40.js
+++
@@ -1,2 +0,0 @@
-import{d as e}from"./singletons.9dd412a2.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
-//# sourceMappingURL=stores.86ec1a40.js.map
---
+++ _app/immutable/chunks/zh.67e41b14.js
@@ -0,0 +1,2 @@
+const e="选择要用哪些地址收币",c="确定",t="创建一个新的钱包",o="1 年",s="可动用:",a={receive:e,ok:c,create:t,year:o,spendable:s};export{t as create,a as default,c as ok,e as receive,s as spendable,o as year};
+//# sourceMappingURL=zh.67e41b14.js.map
