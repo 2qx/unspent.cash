7f4e65d5736db09909c1c1781bb22db6368decc0
2qx <2qx@users.noreply.github.com>
2023-09-23 21:47:56 +0000

    deploy: 2qx/unspent@acf9cc77080202e0ecd60766053fd5e866c25a9d

--- _app/immutable/chunks/stores.8a168142.js
+++
@@ -1,2 +0,0 @@
-import{d as e}from"./singletons.4ce4e1c7.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
-//# sourceMappingURL=stores.8a168142.js.map
