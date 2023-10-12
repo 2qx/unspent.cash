affd12c782c9514b5e2077cb8e54f577ea2efcec
2qx <2qx@users.noreply.github.com>
2023-10-12 17:57:45 +0000

    deploy: 2qx/unspent@d48714d77a45c35da2948ea70027136258c82c6b

--- _app/immutable/chunks/paths.35b34cbb.js
+++
@@ -1,2 +0,0 @@
-const s=globalThis.__sveltekit_jtr6qj?.base??"",t=globalThis.__sveltekit_jtr6qj?.assets??s;export{t as a,s as b};
-//# sourceMappingURL=paths.35b34cbb.js.map
---
+++ _app/immutable/chunks/stores.f7a32e94.js
@@ -0,0 +1,2 @@
+import{d as e}from"./singletons.179762f5.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
+//# sourceMappingURL=stores.f7a32e94.js.map
