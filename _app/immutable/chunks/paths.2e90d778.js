b6bebd6b51c5b64e98d0318693a6a508802a11ba
2qx <2qx@users.noreply.github.com>
2023-10-31 19:00:20 +0000

    deploy: 2qx/unspent@cb2241a28723ddab4c1b4a63022ec11d8cc51439

---
+++ _app/immutable/chunks/paths.2e90d778.js
@@ -0,0 +1,2 @@
+const s=globalThis.__sveltekit_1imooif?.base??"",a=globalThis.__sveltekit_1imooif?.assets??s;export{a,s as b};
+//# sourceMappingURL=paths.2e90d778.js.map
--- _app/immutable/chunks/paths.3d5c6ebe.js
+++
@@ -1,2 +0,0 @@
-const s=globalThis.__sveltekit_jnrbe8?.base??"",e=globalThis.__sveltekit_jnrbe8?.assets??s;export{e as a,s as b};
-//# sourceMappingURL=paths.3d5c6ebe.js.map
