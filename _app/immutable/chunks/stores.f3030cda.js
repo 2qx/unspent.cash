4a469164927b824cc21b235bd0a378042ed1268a
2qx <2qx@users.noreply.github.com>
2023-12-20 17:56:07 +0000

    deploy: 2qx/unspent@5a6ad8f8ee4959ac2c9ec79fd9b7256fdfe84834

--- _app/immutable/chunks/stores.f3030cda.js
+++
@@ -1,2 +0,0 @@
-import{d as e}from"./singletons.d54a2a27.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
-//# sourceMappingURL=stores.f3030cda.js.map
