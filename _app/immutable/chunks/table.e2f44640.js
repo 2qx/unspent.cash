b91bd5ab35fdadb0e34cad6af9f56160aad60a2e
2qx <2qx@users.noreply.github.com>
2023-12-28 23:09:28 +0000

    deploy: 2qx/unspent@c9a423dd1efb684cdae63fc34121285d6041585f

---
+++ _app/immutable/chunks/table.e2f44640.js
@@ -0,0 +1,2 @@
+import{j as t}from"./singletons.dcf4b8b3.js";const e=t("goto"),o=""+new URL("../assets/chart.54b42f49.svg",import.meta.url).href,a=""+new URL("../assets/table.88901b65.svg",import.meta.url).href;export{o as c,e as g,a as t};
+//# sourceMappingURL=table.e2f44640.js.map
