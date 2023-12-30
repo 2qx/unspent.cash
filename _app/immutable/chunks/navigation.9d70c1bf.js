a658ff00ea3012dce15976c0856263577d3fecba
2qx <2qx@users.noreply.github.com>
2023-12-30 19:21:58 +0000

    deploy: 2qx/unspent@ec72bb5986aa9b00196002cfc11f6648a77b192f

--- _app/immutable/chunks/navigation.9d70c1bf.js
+++
@@ -1,2 +0,0 @@
-import{j as o}from"./singletons.732c9131.js";const e=o("goto");export{e as g};
-//# sourceMappingURL=navigation.9d70c1bf.js.map
---
+++ _app/immutable/chunks/navigation.ceb5d1f6.js
@@ -0,0 +1,2 @@
+import{j as o}from"./singletons.56357b96.js";const e=o("goto");export{e as g};
+//# sourceMappingURL=navigation.ceb5d1f6.js.map
